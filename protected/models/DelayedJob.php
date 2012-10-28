fb70bc2005cc8296751d1f49c7920db421f07c27
Tudor Sandu <tm.sandu@gmail.com>
2012-10-28 08:36:14 +0200

    Implemented lock logic. Untested

--- protected/models/DelayedJob.php
+++ protected/models/DelayedJob.php
@@ -9,10 +9,13 @@
  * @property string $type
  * @property string $content
  * @property string $status
+ * @property timestamp $locked_at
+ * @property integer $process_period
  */
 class DelayedJob extends CActiveRecord
 {
 	private $_content = array();
+	protected $_process_period = 60; // In seconds. Override in sub classes
 	
 	/**
 	 * Returns the static model of the specified AR class.
@@ -60,13 +63,13 @@ class DelayedJob extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('account_id, type', 'required'),
-			array('account_id', 'numerical', 'integerOnly'=>true),
+			array('account_id, type, locked_at, process_period', 'required'),
+			array('account_id, process_period', 'numerical', 'integerOnly'=>true),
 			array('type', 'length', 'max'=>16),
 			array('status', 'length', 'max'=>7),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('id, account_id, type, content, status', 'safe', 'on'=>'search'),
+			array('id, account_id, type, content, status, locked_at, process_period', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -99,6 +102,8 @@ class DelayedJob extends CActiveRecord
 			'type' => 'Type',
 			'content' => 'Content',
 			'status' => 'Status',
+			'locked_at' => 'Locked At',
+			'process_period' => 'Process Period'
 		);
 	}
 
@@ -118,6 +123,8 @@ class DelayedJob extends CActiveRecord
 		$criteria->compare('type',$this->type,true);
 		$criteria->compare('content',$this->content,true);
 		$criteria->compare('status',$this->status,true);
+		$criteria->compare('locked_at',$this->locked_at,true);
+		$criteria->compare('process_period',$this->process_period,true);
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
@@ -152,16 +159,25 @@ class DelayedJob extends CActiveRecord
 		return true;
 	}
 	
+	public function lockExpired() {
+		return $this->status == 'started' && (($this->locked_at + $this->process_period) > time());
+	}
+	
 	protected function startProcessing() {
+		if($this->status != 'new' || !$this->lockExpired())
+			return false;
 		$this->status = 'started';
+		$this->locked_at = time();
+		$this->process_period = $this->_process_period;
 		if(!$this->save()) {
 			Yii::log('Unable to start job '.$this->id.': '.CVarDumper::dumpAsString($this->errors));
-			throw new CException('Unable to start job');
+			return false;
 		}
 	}
 	
 	public function process() {
-		$this->startProcessing();
+		if(!$this->startProcessing())
+			return false;;
 		$ok = $this->getTypedDelayedJob()->process();
 		$this->endProcessing();
 		return $ok;
